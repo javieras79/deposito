402a8f8c0c3c956e49a2b267b1ac7abc793496e6
javieras <javieras@ELWSJAVIERAS.elche.inditex.grp>
2018-02-14 19:56:22 +0100

    Nuevos cambios

--- DepositoCombustible/Test/ParametrizacionDCombustible.java
+++ DepositoCombustible/Test/ParametrizacionDCombustible.java
@@ -15,24 +15,25 @@ public class ParametrizacionDCombustible {
 
 	private double depMax;	//propiedades para las pruebas	
 	private double depNivel;     //propiedades para las pruebas
+	private double consumo;
 	
 	DepositoCombustible pdc = new DepositoCombustible(40,depNivel);
 	
-	public ParametrizacionDCombustible(double n, double m)  //Constructor necesario para la prueba
+	public ParametrizacionDCombustible(double n, double m, double c)  //Constructor necesario para la prueba
 	{
 		this.depMax = n;
 		this.depNivel = m;		
+		this.consumo = c;
 	}
 	
 	@Parameters    //En este metodo introduciremos  la bateria de pruebas	
 	public static Collection<Object[]> pruebasConsumo()	{
 		//Bateria de pruebas a realizar
-		return Arrays.asList( new Object[][]{{40,20}} );
+		return Arrays.asList( new Object[][]{{40,20,10}} );		
 	}
 	
 	@Test
-	public void testGetDepositoNivel() {
-		pdc.fill(depNivel);
+	public void testGetDepositoNivel() {		
 		double resultado = pdc.getDepositoNivel();		
 		assertEquals(depNivel,resultado,0);	
 	}
