94c68959a6f64ee5ab46c9cf20819c35dcfe1c7f
javieras <javieras@ELWSJAVIERAS.elche.inditex.grp>
2018-02-14 20:17:47 +0100

    Nuevo cambio

--- DepositoCombustible/Test/ParametrizacionDCombustible.java
+++ DepositoCombustible/Test/ParametrizacionDCombustible.java
@@ -3,11 +3,14 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
 
 @RunWith(Parameterized.class)
 
@@ -15,44 +18,45 @@ public class ParametrizacionDCombustible {
 
 	private double depMax;	//propiedades para las pruebas	
 	private double depNivel;     //propiedades para las pruebas
-	private double consumo;
-	
-	DepositoCombustible pdc = new DepositoCombustible(40,depNivel);
-	
-	public ParametrizacionDCombustible(double n, double m, double c)  //Constructor necesario para la prueba
+		
+	public ParametrizacionDCombustible(double n, double m)  //Constructor necesario para la prueba
 	{
 		this.depMax = n;
 		this.depNivel = m;		
-		this.consumo = c;
+		
 	}
 	
 	@Parameters    //En este metodo introduciremos  la bateria de pruebas	
 	public static Collection<Object[]> pruebasConsumo()	{
 		//Bateria de pruebas a realizar
-		return Arrays.asList( new Object[][]{{40,20,10}} );		
+		return Arrays.asList( new Object[][]{{30,15}} );		
 	}
 	
 	@Test
 	public void testGetDepositoNivel() {		
-		double resultado = pdc.getDepositoNivel();		
-		assertEquals(depNivel,resultado,0);	
+		DepositoCombustible pdc = new DepositoCombustible(depMax,depNivel);	
+		double resultado = pdc.getDepositoNivel();			
+		assertEquals(depNivel,resultado,0);		
 	}
 	
 	@Test
 	public void testGetDepositoMax() {			
+		DepositoCombustible pdc = new DepositoCombustible(depMax,depNivel);
 		double resultado = pdc.getDepositoMax();
 		assertEquals(depMax,resultado,0);
 	}
 
 	@Test
-	public void testEstaVacio() {
+	public void testEstaVacio() {		
+		DepositoCombustible pdc = new DepositoCombustible(depMax,depNivel);
 		boolean vacio = pdc.estaVacio();
 		if (depNivel != 0)		
 		assertTrue(false);		
 	}	
 	
 	@Test
-	public void testDepositoMitad() {
+	public void testDepositoMitad() {		
+		DepositoCombustible pdc = new DepositoCombustible(depMax,depNivel);
 		double mitad = pdc.getDepositoMax()/2;
 		assertEquals(depNivel,mitad,0);
 	}	
